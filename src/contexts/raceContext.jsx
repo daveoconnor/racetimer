81acba22d86748c5772bfaf01166b29bc5d0b636
Dave O'Connor <doc+gitkraken@skynet.ie>
2019-08-27 00:49:42 -0700

    Changed race context to pass in event times and other minor changes

--- src/contexts/raceContext.jsx
+++ src/contexts/raceContext.jsx
@@ -1,8 +1,9 @@
-import React, { useReducer } from 'react';
+import React, { useCallback, useContext, useReducer } from 'react';
+import _ from 'lodash';
 
 export const RaceContext = React.createContext();
 
-const reducer = (state, action) => {
+const raceReducer = (state, action) => {
 	const createBoat = (boatNumber, startTime) => {
 		return {
 			boatNumber: boatNumber,
@@ -14,43 +15,65 @@ const reducer = (state, action) => {
 		const startTime = Date.now();
 		const boats = {}
 		for (let i = 1; i < action.payload.numberOfBoats + 1; i++) {
-			boats[i] = (createBoat(i, startTime));
+			boats[i] = createBoat(i, startTime);
 		}
 		return boats;
 	}
 	switch(action.type) {
 		case 'startAllBoats':
-			const allBoats = createBoats(action.payload.numberOfBoats);
+			const numberOfBoats = action.payload.numberOfBoats;
+			const allStartTimes = action.payload.startTime;
+			const allBoats = createBoats(numberOfBoats, allStartTimes);
 			return { ...state, boats: allBoats };
 		case 'startBoat':
 			const boatNumber = action.payload.boatNumber;
+			const boatStartTime = action.payload.startTime;
 			const boats = state.boats;
-			boats[boatNumber] = createBoat(boatNumber, Date.now());
+			const newBoat = createBoat(boatNumber, boatStartTime);
+			boats[boatNumber] = newBoat;
 			return { ...state, boats: boats };
 		case 'stopBoat':
 			const existingBoat = state.boats[action.payload.boatNumber];
-			existingBoat.stopTime = Date.now()
+			existingBoat.stopTime = action.payload.stopTime;
 			return {...state};
 		default:
-			return {...state};
+			return state;
 	}
-}
+};
 
 export default function RaceProvider({client, children}) {
 	const defaultState = {
 		boats: {}
 	};
-	const [state, dispatch] = useReducer(reducer, defaultState);
+	const [state, dispatch] = useReducer(raceReducer, defaultState);
 	const startBoat = (boatNumber) => {
-		dispatch({type: 'startBoat', payload: { boatNumber: boatNumber }})
+		dispatch({
+			type: 'startBoat',
+			payload: {
+				boatNumber: boatNumber,
+				startTime: Date.now()
+			}
+		});
 	}
 
 	const startAllBoats = (numberOfBoats) => {
-		dispatch({type: 'startAllBoats', payload: { numberOfBoats: 6 }})
+		dispatch({
+			type: 'startAllBoats',
+			payload: {
+				numberOfBoats: 6,
+				startTime: Date.now()
+			}
+		});
 	}
 
 	const stopBoat = (boatNumber) => {
-		dispatch({type: 'stopBoat', payload: { boatNumber: boatNumber }})
+		dispatch({
+			type: 'stopBoat',
+			payload: {
+				boatNumber: boatNumber,
+				stopTime: Date.now()
+			}
+		});
 	}
 
 	return (

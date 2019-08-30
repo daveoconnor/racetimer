b1a13a319c2a2490a49b5a9b2571adddc30068d9
Dave O'Connor <doc+gitkraken@skynet.ie>
2019-08-30 16:08:26 -0700

    Make logging data in localStorage be date based

--- src/contexts/logContext.jsx
+++ src/contexts/logContext.jsx
@@ -4,10 +4,21 @@ export const LogContext = React.createContext();
 
 export default function LogProvider({client, children}) {
 	const [logs, setLogs] = useState([]);
+	const [backupName, setBackupName ] = useState('log-backup-' + Date.now());
 
 	const log = async (message) => {
 		logs.push(message);
 		setLogs([...logs]);
+		backupLog(message);
+		return 1;
+	}
+
+	const backupLog = async () => {
+		let logData = JSON.parse(localStorage.getItem('logs'));
+		if (!logData) {
+			logData = [];
+		}
+		localStorage.setItem(backupName, JSON.stringify(logs));
 		return 1;
 	}
 
